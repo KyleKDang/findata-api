fc763273769b3868117083bd73e70f5ea1907157
Kyle Dang <kylekd1@uci.edu>
2025-12-23 15:52:16 -0800

    refactor: renamed FindataApiApplication to FinDataApiApplication

--- src/main/java/com/findata/api/FinDataApiApplication.java
+++ src/main/java/com/findata/api/FinDataApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FindataApiApplication {
+public class FinDataApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FindataApiApplication.class, args);
+		SpringApplication.run(FinDataApiApplication.class, args);
 	}
 
 }
