6bc8b3af3bbe8e87218b3c3e024a31513ca94ea3
Kyle Dang <kylekd1@uci.edu>
2025-12-23 15:44:34 -0800

    chore: initialize Spring Boot 3.5.9 project with Maven

---
+++ src/main/java/com/findata/api/FindataApiApplication.java
@@ -0,0 +1,13 @@
+package com.findata.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findata/api/FindataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findata.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
