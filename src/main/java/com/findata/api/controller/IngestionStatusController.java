7e94de714083caac2a728e9470ca56f3d3b60d9a
Kyle Dang <kylekd1@uci.edu>
2026-03-09 23:28:40 -0700

    fix: add GET mapping for latest job endpoint

--- src/main/java/com/findata/api/controller/IngestionStatusController.java
+++ src/main/java/com/findata/api/controller/IngestionStatusController.java
@@ -4,6 +4,7 @@ import com.findata.api.model.entity.IngestionStatus;
 import com.findata.api.repository.IngestionStatusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ public class IngestionStatusController {
 
     private final IngestionStatusRepository ingestionStatusRepository;
 
+    @GetMapping("/status/latest")
     public ResponseEntity<IngestionStatus> getLatestJobStatus() {
         return ingestionStatusRepository.findFirstByOrderByJobStartedAtDesc()
                 .map(ResponseEntity::ok)
