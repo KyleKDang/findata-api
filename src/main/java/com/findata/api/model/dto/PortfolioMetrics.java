f7d5745e04e64b2b0ba7d7ca361ceb66642354ac
Kyle Dang <kylekd1@uci.edu>
2026-03-20 14:38:16 -0700

    feat: add PortfolioMetrics response DTO

---
+++ src/main/java/com/findata/api/model/dto/PortfolioMetrics.java
@@ -0,0 +1,40 @@
+package com.findata.api.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PortfolioMetrics {
+
+    private List<PortfolioPosition> positions;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private BigDecimal portfolioReturn;
+    private BigDecimal portfolioReturnPercent;
+    private BigDecimal portfolioVolatility;
+    private BigDecimal portfolioSharpeRatio;
+
+    private List<PositionMetrics> positionMetrics;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class PositionMetrics {
+        private String ticker;
+        private BigDecimal weight;
+        private BigDecimal returnPercent;
+        private BigDecimal volatility;
+        private BigDecimal contribution;
+    }
+}
